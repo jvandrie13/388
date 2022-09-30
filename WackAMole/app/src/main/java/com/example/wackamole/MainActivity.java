e84abb52bbb0da0aea5f0ec56f06839546a8e312
Van Drie Joshua D <VanDrieJoshuaD@JohnDeere.com>
2022-09-30 13:12:02 -0500

    Fuck the Queen

--- WackAMole/app/src/main/java/com/example/wackamole/MainActivity.java
+++ WackAMole/app/src/main/java/com/example/wackamole/MainActivity.java
@@ -1,14 +1,45 @@
 package com.example.wackamole;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView highScore;
+    private TextView prevScore;
+    private MainActivityViewModel model;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        highScore = findViewById(R.id.high_score);
+        prevScore = findViewById(R.id.prev_score);
+        model = new ViewModelProvider(this).get(MainActivityViewModel.class);
+
+        final Observer<Integer> scoreObserver = new Observer<Integer>() {
+            @Override
+            public void onChanged(@Nullable final Integer newScore) {
+                highScore.setText(model.getHighScore().getValue().toString());
+                prevScore.setText(model.getPrevScore().getValue().toString());
+            }
+        };
+
+        model.getHighScore().observe(this, scoreObserver);
+        model.getPrevScore().observe(this, scoreObserver);
+
+    }
+
+    public void onClickPlay(View view)
+    {
+
     }
 }
---
+++ WackAMole/app/src/main/java/com/example/wackamole/MainActivityViewModel.java
@@ -0,0 +1,25 @@
+package com.example.wackamole;
+
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class MainActivityViewModel extends ViewModel {
+    public MutableLiveData<Integer> highScore;
+    public MutableLiveData<Integer> prevScore;
+
+    public MutableLiveData<Integer> getHighScore() {
+        if (highScore == null) {
+            highScore = new MutableLiveData<Integer>(0);
+            highScore.setValue(0);
+        }
+        return highScore;
+    }
+
+    public MutableLiveData<Integer> getPrevScore() {
+        if (prevScore == null) {
+            prevScore = new MutableLiveData<Integer>(0);
+            prevScore.setValue(0);
+        }
+        return prevScore;
+    }
+}
