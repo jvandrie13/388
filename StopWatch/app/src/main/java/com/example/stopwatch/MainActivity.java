f1a0d944eb4489acc9cdef40dfd1082f16b4b5fe
Van Drie Joshua D <VanDrieJoshuaD@JohnDeere.com>
2022-09-30 11:06:50 -0500

    hi shayla

--- StopWatch/app/src/main/java/com/example/stopwatch/MainActivity.java
+++ StopWatch/app/src/main/java/com/example/stopwatch/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.stopwatch;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModel;
 import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
@@ -15,10 +18,10 @@ import java.util.Locale;
 public class MainActivity extends AppCompatActivity {
 
     private int milliseconds = 0;
-    private boolean running = false;
-    private MutableLiveData<Long> elapsedTime = new MutableLiveData<Long>(0L);
+//    private boolean running = false;
     TextView StartStop;
-    MyViewModel model;
+    public TextView timeView;
+    private MyViewModel model;
 
 
     @Override
@@ -26,45 +29,65 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         StartStop = findViewById(R.id.StartStop);
+        timeView = findViewById(R.id.time);
         model = new ViewModelProvider(this).get(MyViewModel.class);
+
+        final Observer<Integer> timeObserver = new Observer<Integer>() {
+            @Override
+            public void onChanged(@Nullable final Integer newTime) {
+                long milli = model.getCurrentTime().getValue() % 100;
+                long secs = (model.getCurrentTime().getValue() / 100) %60;
+                long minutes = (model.getCurrentTime().getValue() / 6000) %60;
+                long hours = model.getCurrentTime().getValue() / 360000;
+
+
+                String timeString = String.format(Locale.getDefault(), "%d:%02d:%02d.%01d", hours, minutes, secs, milli);
+                timeView.setText(timeString);
+            }
+        };
+
+        final Observer<String> StartStopObserver = new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable final String newString) {
+                StartStop.setText(model.getStartStop().getValue());
+            }
+        };
+
+        model.getCurrentTime().observe(this, timeObserver);
+        model.getStartStop().observe(this, StartStopObserver);
+        milliseconds = model.getCurrentTime().getValue();
+        StartStop.setText(model.getStartStop().getValue());
         runTimer();
     }
 
     public void onClickStart(View view)
     {
-        if (running){
-            StartStop.setText(R.string.start);
+        if (model.getIsRunning().getValue() == false){
+            model.getStartStop().setValue("Stop");
         } else {
-            StartStop.setText(R.string.stop);
+            model.getStartStop().setValue("Start");
         }
-        running = !running;
+        model.getIsRunning().setValue(!model.getIsRunning().getValue());
     }
 
     public void onClickReset(View view)
     {
         milliseconds = 0;
+        model.getCurrentTime().setValue(0);
+
     }
 
     private void runTimer(){
 
-        TextView timeView = findViewById(R.id.time);
         Handler handler = new Handler();
         handler.postAtTime(new Runnable() {
             @Override
             public void run() {
-                if (running) {
+                if (model.getIsRunning().getValue()) {
                     milliseconds = milliseconds + 10;
                 }
-                elapsedTime.setValue((long) milliseconds);
-                long milli = elapsedTime.getValue() % 100;
-                long secs = (elapsedTime.getValue() / 100) %60;
-                long minutes = (elapsedTime.getValue() / 60000) %60;
-                long hours = elapsedTime.getValue() / 3600000;
+                model.getCurrentTime().setValue(milliseconds);
 
-;
-                String timeString = String.format(Locale.getDefault(), "%d:%02d:%02d.%01d", hours, minutes, secs, milli);
-
-                timeView.setText(timeString);
                 handler.postDelayed(this, 100);
             }
         },SystemClock.uptimeMillis());
--- StopWatch/app/src/main/java/com/example/stopwatch/MyViewModel.java
+++ StopWatch/app/src/main/java/com/example/stopwatch/MyViewModel.java
@@ -11,50 +11,35 @@ import androidx.lifecycle.ViewModel;
 import java.util.Locale;
 
 public class MyViewModel extends ViewModel {
-    private int milliseconds = 0;
-
-    public void setMilliseconds(int milliseconds) {
-        this.milliseconds = milliseconds;
-    }
-
-    private boolean running = false;
-
-    public void setRunning() {
-        this.running = !running;
-    }
+    public MutableLiveData<Integer> time;
+    public MutableLiveData<Boolean> running;
+    public MutableLiveData<String> StartStop;
 
 
-    private MutableLiveData<Long> elapsedTime;
-
-    public LiveData<Long> getElapsedTime() {
-        if (elapsedTime == null) {
-            elapsedTime = new MutableLiveData<Long>(0L);
-            updateTime();
+    public MutableLiveData<Integer> getCurrentTime() {
+        if (time == null) {
+            time = new MutableLiveData<Integer>();
+            time.setValue(0);
         }
-        return elapsedTime;
+        return time;
     }
 
-    private void updateTime() {
-//        TextView timeView = findViewById(R.id.time);
-        Handler handler = new Handler();
-        handler.postAtTime(new Runnable() {
-            @Override
-            public void run() {
-                if (running) {
-                    milliseconds = milliseconds + 10;
-                }
-                elapsedTime.setValue((long) milliseconds);
-                long milli = elapsedTime.getValue() % 100;
-                long secs = (elapsedTime.getValue() / 100) %60;
-                long minutes = (elapsedTime.getValue() / 60000) %60;
-                long hours = elapsedTime.getValue() / 3600000;
+    public MutableLiveData<Boolean> getIsRunning() {
+        if (running == null) {
+            running = new MutableLiveData<Boolean>();
+            running.setValue(false);
+        }
+        return running;
+    }
 
-                ;
-                String timeString = String.format(Locale.getDefault(), "%d:%02d:%02d.%01d", hours, minutes, secs, milli);
+    public MutableLiveData<String> getStartStop() {
+        if (StartStop == null) {
+            StartStop = new MutableLiveData<String>();
+            StartStop.setValue("Start");
+        }
+        return StartStop;
+    }
 
-//                timeView.setText(timeString);
-                handler.postDelayed(this, 100);
-            }
-        }, SystemClock.uptimeMillis());    }
+
 }
 
