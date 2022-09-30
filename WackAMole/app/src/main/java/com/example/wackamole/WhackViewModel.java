fb6ba5008e2714a17f7f0c3c77f5f939484e513e
Van Drie Joshua D <VanDrieJoshuaD@JohnDeere.com>
2022-09-30 13:54:01 -0500

    WHACK

---
+++ WackAMole/app/src/main/java/com/example/wackamole/WhackViewModel.java
@@ -0,0 +1,24 @@
+package com.example.wackamole;
+
+import androidx.lifecycle.MutableLiveData;
+
+public class WhackViewModel {
+    public MutableLiveData<Integer> currentScore;
+    public MutableLiveData<Integer> lives;
+    
+    public MutableLiveData<Integer> getCurrentScore() {
+        if (currentScore == null) {
+            currentScore = new MutableLiveData<Integer>(0);
+            currentScore.setValue(0);
+        }
+        return currentScore;
+    }
+
+    public MutableLiveData<Integer> getLives() {
+        if (lives == null) {
+            lives = new MutableLiveData<Integer>(3);
+            lives.setValue(3);
+        }
+        return lives;
+    }
+}
