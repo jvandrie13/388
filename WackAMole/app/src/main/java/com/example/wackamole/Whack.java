d92c23ab8edd5621144608404f199b4a4086e02d
Van Drie Joshua D <VanDrieJoshuaD@JohnDeere.com>
2022-10-04 14:55:02 -0500

    Fuck a mole

--- WackAMole/app/src/main/java/com/example/wackamole/Whack.java
+++ WackAMole/app/src/main/java/com/example/wackamole/Whack.java
@@ -1,14 +1,83 @@
 package com.example.wackamole;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.SystemClock;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.wackamole.Main.MainActivityViewModel;
+
+import java.util.Random;
 
 public class Whack extends AppCompatActivity {
+    private TextView highScore;
+    private TextView currScore;
+    private TextView livesLeft;
+    private MainActivityViewModel model;
+    Handler handler;
+    Runnable runnable;
+    ImageView[] moles;
+    boolean clicked;
+    Integer lives;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main2);
+        setContentView(R.layout.whack);
+        highScore = findViewById(R.id.whack_high_score);
+        currScore = findViewById(R.id.whack_score);
+        livesLeft = findViewById(R.id.whack_lives);
+        moles = new ImageView[9];
+
+        for (int i = 0; i < 9; i++) {
+            int res = getResources().getIdentifier("mole_"+i, "id", getPackageName()); //This line is necessary to "convert" a string (e.g. "i1", "i2" etc.) to a resource ID
+            moles[i] = (ImageView) findViewById(res);
+            moles[i].setVisibility(View.INVISIBLE);
+        }
+
+        clicked = false;
+        lives = 3;
+        livesLeft.setText(lives.toString());
+
+        handler = new Handler();
+        runnable = new Runnable() {
+            @Override
+            public void run() {
+                int prevMole = model.getVisibleMole().getValue();
+                moles[prevMole].setVisibility(View.INVISIBLE);
+                int random = new Random().nextInt(9);
+                model.getVisibleMole().setValue(random);
+                moles[random].setVisibility(View.VISIBLE);
+                handler.postDelayed(this, 1000);
+            }
+        };
+
+        model = new ViewModelProvider(this).get(MainActivityViewModel.class);
+        final Observer<Integer> scoreObserver = new Observer<Integer>() {
+            @Override
+            public void onChanged(@Nullable final Integer newScore) {
+                highScore.setText(model.getHighScore().getValue().toString());
+                currScore.setText(model.getCurrScore().getValue().toString());
+            }
+        };
+
+        model.getHighScore().observe(this, scoreObserver);
+        model.getCurrScore().observe(this, scoreObserver);
+
+        handler.postAtTime(runnable, SystemClock.uptimeMillis());
+    }
+
+    public void onClickMole(View view){
+        model.getCurrScore().setValue(model.getCurrScore().getValue()+10);
+        moles[model.getVisibleMole().getValue()].setVisibility(View.INVISIBLE);
+        clicked = true;
     }
 }
