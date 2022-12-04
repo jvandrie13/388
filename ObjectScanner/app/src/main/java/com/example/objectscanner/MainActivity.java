63ff1378a5c236bf546086b46ded29832c843222
Van Drie Joshua D <VanDrieJoshuaD@JohnDeere.com>
2022-12-04 14:42:20 -0600

    2048 test

--- ObjectScanner/app/src/main/java/com/example/objectscanner/MainActivity.java
+++ ObjectScanner/app/src/main/java/com/example/objectscanner/MainActivity.java
@@ -1,14 +1,162 @@
 package com.example.objectscanner;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.camera.core.Camera;
+import androidx.camera.core.CameraSelector;
+import androidx.camera.core.ImageAnalysis;
+import androidx.camera.core.ImageProxy;
+import androidx.camera.core.Preview;
+import androidx.camera.core.UseCase;
+import androidx.camera.lifecycle.ProcessCameraProvider;
+import androidx.camera.view.PreviewView;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.camera.lifecycle.ProcessCameraProvider;
+import androidx.lifecycle.LifecycleOwner;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.common.util.concurrent.ListenableFuture;
+import com.google.mlkit.vision.common.InputImage;
+import com.google.mlkit.vision.label.ImageLabel;
+import com.google.mlkit.vision.label.ImageLabeler;
+import com.google.mlkit.vision.label.ImageLabeling;
+import com.google.mlkit.vision.label.defaults.ImageLabelerOptions;
+
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.media.Image;
 import android.os.Bundle;
+import android.renderscript.ScriptGroup;
+import android.util.Size;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executors;
+
 
 public class MainActivity extends AppCompatActivity {
+    private PreviewView previewView;
+    private Preview preview;
+    private ListenableFuture<ProcessCameraProvider> cameraProviderFuture;
+    TextView analysis;
+    Camera camera;
+    ImageLabeler labeler;
+    String onCam;
+    ArrayList<String> onCamList;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        cameraProviderFuture = ProcessCameraProvider.getInstance(this);
+        checkCameraProvider();
+        previewView = findViewById(R.id.previewView);
+        analysis = findViewById(R.id.analysis);
+        onCam="";
+        onCamList = new ArrayList<>();
+
+        if (ContextCompat.checkSelfPermission(
+                this, Manifest.permission.CAMERA) ==
+                PackageManager.PERMISSION_GRANTED) {
+        } else {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.CAMERA},
+                    1);
+        }
+
+//        labeler = ImageLabeling.getClient(ImageLabelerOptions.DEFAULT_OPTIONS);
+
+
     }
-}
+
+    void checkCameraProvider(){
+        cameraProviderFuture.addListener(() -> {
+            try {
+                ProcessCameraProvider cameraProvider = cameraProviderFuture.get();
+                bindPreview(cameraProvider);
+            } catch (ExecutionException | InterruptedException e) {
+                // No errors need to be handled for this Future.
+                // This should never be reached.
+            }
+        }, ContextCompat.getMainExecutor(this));
+    }
+
+    void bindPreview(@NonNull ProcessCameraProvider cameraProvider) {
+        preview = new Preview.Builder()
+                .build();
+
+        CameraSelector cameraSelector = new CameraSelector.Builder()
+                .requireLensFacing(CameraSelector.LENS_FACING_BACK)
+                .build();
+
+        preview.setSurfaceProvider(previewView.getSurfaceProvider());
+        setupImageAnalysis(cameraSelector, cameraProvider);
+//        camera = cameraProvider.bindToLifecycle((LifecycleOwner)this, cameraSelector, preview);
+    }
+
+    void setText(String text){
+        analysis.setText(text);
+    }
+    void setupImageAnalysis(CameraSelector cameraSelector, ProcessCameraProvider cameraProvider){
+        ImageAnalysis imageAnalysis =
+                new ImageAnalysis.Builder()
+                        // enable the following line if RGBA output is needed.
+                        //.setOutputImageFormat(ImageAnalysis.OUTPUT_IMAGE_FORMAT_RGBA_8888)
+                        .setTargetResolution(new Size(1280, 720))
+                        .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
+                        .build();
+
+        imageAnalysis.setAnalyzer(Executors.newSingleThreadExecutor(), new ImageAnalysis.Analyzer() {
+            @Override
+            public void analyze(@NonNull ImageProxy imageProxy) {
+                int rotationDegrees = imageProxy.getImageInfo().getRotationDegrees();
+                // insert your code here.
+                Image mediaImage = imageProxy.getImage();
+                InputImage image = null;
+                if (mediaImage != null) {
+                    image = InputImage.fromMediaImage(mediaImage, imageProxy.getImageInfo().getRotationDegrees());
+                }
+                labeler = ImageLabeling.getClient(ImageLabelerOptions.DEFAULT_OPTIONS);
+                labeler.process(image)
+                        .addOnSuccessListener(new OnSuccessListener<List<ImageLabel>>() {
+                            @Override
+                            public void onSuccess(List<ImageLabel> labels) {
+                                // Task completed successfully
+                                onCam = "";
+                                for (ImageLabel label : labels) {
+                                    String text = label.getText();
+                                    float confidence = label.getConfidence();
+                                    int index = label.getIndex();
+                                    System.out.println(text);
+                                    onCam = onCam + text + " , " + confidence + " , " + index + "\n";
+                                }
+                                setText(onCam);
+
+
+                                imageProxy.close();
+                            }
+                        })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                // Task failed with an exception
+                                imageProxy.close();
+                            }
+                        });
+                // after done, release the ImageProxy object
+            }
+        });
+
+        cameraProvider.bindToLifecycle((LifecycleOwner) this, cameraSelector, imageAnalysis, preview);
+//        analysis.setText(onCam);
+    };
+};
+
+
